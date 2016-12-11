81ff3759ed0da429bf1b62c4a79bc8b63404f4c6
cgcodeboy <1610737331@qq.com>
2016-12-11 21:13:24 +0800

    some extra commit

---
+++ vtkCurvatures-build/CMakeFiles/vtkCurvatures.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtkCurvatures.dir/vtkCurvatures.cpp.obj"
+  "vtkCurvatures.pdb"
+  "vtkCurvatures.exe"
+  "vtkCurvatures.exe.manifest"
+  "libvtkCurvatures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtkCurvatures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ vtkPolyDataNormal-build/CMakeFiles/vtkPolyDataNormal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtkPolyDataNormal.dir/vtkPolyDataNormal.cpp.obj"
+  "vtkPolyDataNormal.pdb"
+  "vtkPolyDataNormal.exe"
+  "vtkPolyDataNormal.exe.manifest"
+  "libvtkPolyDataNormal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtkPolyDataNormal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
